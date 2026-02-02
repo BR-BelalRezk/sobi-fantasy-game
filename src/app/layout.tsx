74b85b5f119d260a1e2cc5cdfd91f187e158a973
BR-BelalRezk <belalrezk.developer@gmail.com>
2026-02-03 01:09:26 +0200

    feat: Initialize Next.js App Router project with PNPM, Tailwind CSS, and core utilities.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "@/styles/globals.css";
+import { cn } from "@/utils";
 
 export const metadata: Metadata = {
   title: "SOBI Fantasy Game",
@@ -13,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased`}>{children}</body>
+      <body className={cn("antialiased")}>{children}</body>
     </html>
   );
 }
