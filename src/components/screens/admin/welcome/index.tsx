93046e0cb510d0daca4833c7aa95f5a6458af172
BR-BelalRezk <belalrezk.developer@gmail.com>
2026-02-05 18:50:24 +0200

    next

--- src/components/screens/admin/welcome/index.tsx
+++ src/components/screens/admin/welcome/index.tsx
@@ -43,7 +43,7 @@ export default function Welcome() {
             transition={{ duration: 0.8, ease: [0.16, 1, 0.3, 1] }}
           >
             <GameButton
-              text="Start"
+              text="Next"
               onClick={() => setPhase("beforeSpeedQuestion")}
             />
           </motion.div>
